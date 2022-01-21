4c887db968f9ad57305c194fc13c91e50d950d1b
lihaibin <lihaibin@tangbull.com>
2022-01-21 19:00:24 +0800

    add queue

--- sql.py
+++ sql.py
@@ -11,7 +11,7 @@
 
 import pymysql
 import time
-
+import uuid
 conn = pymysql.connect(
     host='192.168.50.25',
     port=3306,
@@ -22,13 +22,17 @@ conn = pymysql.connect(
 
 def insert_data(numberlist):
     create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    uuid_str = uuid.uuid4()
     for number in numberlist:
-        sql = 'INSERT INTO record(number,idle,create_time) VALUES("%s","%s","%s")' % (number, "True",create_time)
+        sql = 'INSERT INTO record(number,idle,create_time,uuid) VALUES("%s","%s","%s")' % (number, "True",create_time,uuid_str)
         cur = conn.cursor()
         cur.execute(sql)
     cur.close()
     conn.commit()
     conn.close()
+
+def update_data():
+    pass
 
 def get_data(task_id):
 
